0f7c362d3188b2ed31a9e8c50f7b05f9b908ae3d
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-08-16 16:31:29 -0400

    create initial vue project using vue UI tools

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <AppHeader></AppHeader>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    AppHeader
   }
 }
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="app-header">
+        <h1>Local Reviews</h1>
+        <h2>So you can always remember the good {{ current_type }} {{ current_place }}</h2>
+        <h2>Review your favorite local places so you can always remember which is the good pizza place</h2>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data: function () {
+    return {
+      food_types: [
+        'pizza', 'pasta', 'chinese', 'thai'
+      ],
+      place_types: [
+        'place', 'joint', 'spot'
+      ]
+    }
+  },
+  computed: {
+    current_type: function () {
+      return this.food_types[Math.floor(Math.random() * this.food_types.length)]
+    },
+    current_place: function () {
+      return this.place_types[Math.floor(Math.random() * this.place_types.length)]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header.app-header {
+    text-align:center;
+}
+</style>
