4ece8786fcdfa5d443198c6f9560e66e66419a4c
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-08-20 14:46:43 -0400

    add Buefy (vue + bulma) support; add bulma syntax and styling to AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,13 @@
 <template>
     <header class="app-header">
-        <h1>Local Reviews</h1>
-        <h2>So you can always remember the good {{ current_type }} {{ current_place }}</h2>
+      <section class="hero is-medium">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">Local Reviews</h1>
+            <h2 class="subtitle">Your neighborhood, your reviews. So you can <span id="random-predicate" v-on:click="updateRandomPredicate">{{ random_predicate }}</span></h2>
+          </div>
+        </div>
+      </section>
         <input type="text" placeholder="Search by type">
         <input type="text" placeholder="Search by location">
     </header>
@@ -12,20 +18,26 @@ export default {
   name: 'AppHeader',
   data: function () {
     return {
-      food_types: [
-        'pizza', 'pasta', 'chinese', 'thai'
+      predicates: [
+        'find the good pizza place.', 'remember which sub show uses too much mayo.', 'stop at the right barber shop.', 'find the really spicy thai restaurant again.', 'get to the cheap burger stop.', 'remember which theater has the comfy seats.', 'remember the name of that one BBQ place (something about happy pigs...)', 'avoid that bar with re-heated hot pockets.', 'find that dive bar that had the PokeStop outside it.'
       ],
-      place_types: [
-        'place', 'joint', 'spot'
-      ]
+      random_predicate: 'find the good pizza place.'
     }
   },
-  computed: {
-    current_type: function () {
-      return this.food_types[Math.floor(Math.random() * this.food_types.length)]
+  created: function () {
+    this.random_predicate = this.getRandomPredicate();
+  },
+  methods: {
+    updateRandomPredicate: function () {
+      let last_predicate = this.random_predicate;
+      let new_predicate = this.getRandomPredicate();
+      while(last_predicate === new_predicate){
+        new_predicate = this.getRandomPredicate();
+      }
+      this.random_predicate = new_predicate;
     },
-    current_place: function () {
-      return this.place_types[Math.floor(Math.random() * this.place_types.length)]
+    getRandomPredicate: function () {
+      return this.predicates[Math.floor(Math.random() * this.predicates.length)];
     }
   }
 }
@@ -33,6 +45,11 @@ export default {
 
 <style lang="scss" scoped>
 header.app-header {
-    text-align:center;
+  //text-align:center;
+}
+#random-predicate {
+  background-color:#f5f5f5;
+  padding:.25em .5em .25em;
+  font-size:0.875em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
 import './registerServiceWorker'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
