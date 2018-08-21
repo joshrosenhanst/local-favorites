29c3e7f4c34d64e3d30efb9937db2788cda6b1b2
Josh mac <joshrh88@gmail.com>
2018-08-21 15:08:30 -0400

    fix StarRating component: use font icons instead of SVG, add cancel button, add is-hovered functionality (ex highlight stars 1-3 when star 3 is mouseentered)

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,29 +1,35 @@
 <template>
     <div class="star-rating">
-        <!--<span class="star" v-bind:class="[currentStars > 0 ? 'full' : 'empty']" v-on:click="setStars(1)"></span>
-        <span class="star" v-bind:class="[currentStars > 1 ? 'full' : 'empty']" v-on:click="setStars(2)"></span>
-        <span class="star" v-bind:class="[currentStars > 2 ? 'full' : 'empty']" v-on:click="setStars(3)"></span>
-        <span class="star" v-bind:class="[currentStars > 3 ? 'full' : 'empty']" v-on:click="setStars(4)"></span>
-        <span class="star" v-bind:class="[currentStars > 4 ? 'full' : 'empty']" v-on:click="setStars(5)"></span>-->
-        <StarFullIcon class="star empty"></StarFullIcon>
+        <span class="star-rating-icon star"
+            v-for="n in 5"
+            v-bind:key="'star_'+n"
+            v-bind:class="[currentStars >= n ? 'full' : 'empty', currentHover >= n ? 'is-hovered':'']"
+            v-on:click="setStars(n)"
+            v-on:mouseenter="currentHover = n"
+            v-on:mouseleave="currentHover = 0"
+            v-bind:title="'Rate '+n+' stars'"
+        >
+            <font-awesome-icon icon="star" class="star-icon"></font-awesome-icon>
+        </span>
+        <span class="star-rating-icon ban-icon" title="Remove Star Rating"
+            v-if="currentStars"
+            v-on:click="setStars(0)"
+        >
+            <font-awesome-icon icon="ban"></font-awesome-icon>
+        </span>
     </div>
 </template>
 
 <script>
-import StarEmptyIcon from '../assets/star-empty.svg'
-import StarFullIcon from '../assets/star-full.svg'
 export default {
     name: 'StarRating',
     props: {
         stars: Number
     },
-    components: {
-        StarEmptyIcon,
-        StarFullIcon
-    },
     data: function () {
         return {
-            currentStars: 0
+            currentStars: 0,
+            currentHover: 0
         }
     },
     created: function () {
@@ -40,25 +46,28 @@ export default {
 
 
 <style lang="scss" scoped>
-    .star {
-        cursor: pointer;
-        font-weight:bold;
-        display:inline-block;
-        &.empty {
+    .star{
+        &.empty>.star-icon {
             color:grey;
-            fill:grey;
-            //background-image:url(../assets/star-empty.svg);
         }
-        &.full {
+        &.full>.star-icon {
             color:blue;
-            fill:blue;
-            //background-image:url(../assets/star-full.svg);
         }
-        &:hover {
+        &.is-hovered>.star-icon {
             color:green;
-            //background-color:green;
-            fill:green;
         }
+    }
+    .star-rating-icon{
+        cursor: pointer;
+        display:inline-block;
+        margin-right:5px;
+        &:hover{
+            color:red;
+        }
+    }
+    .ban-icon{
+        margin-left:5px;
+        vertical-align: -1px;
     }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faExternalLinkAlt, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { faExternalLinkAlt, faMapMarkedAlt, faStar, faBan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './registerServiceWorker'
 import 'buefy/lib/buefy.css'
 
 Vue.use(Buefy)
-library.add(faExternalLinkAlt, faMapMarkedAlt)
+library.add(faExternalLinkAlt, faMapMarkedAlt, faStar, faBan)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
