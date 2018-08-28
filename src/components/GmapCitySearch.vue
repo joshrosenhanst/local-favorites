a8ca768856c6659c008e0963964972dd98eea586
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-08-28 16:40:14 -0400

    remove old html from GmapCitySearch

--- src/components/GmapCitySearch.vue
+++ src/components/GmapCitySearch.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="field">
     <div class="control has-icons-left">
-      <!--<input type="text" class="input" :value="value" placeholder="Search locations by name, address, or type..." ref="gsearchbox" id="gmap-search-box" />-->
       <input type="text" class="input" :value="value" placeholder="Enter a city..." ref="gmap-autocomplete" id="gmap-autocomplete" />
       <span class="icon is-small is-left">
         <font-awesome-icon icon="search"></font-awesome-icon>
