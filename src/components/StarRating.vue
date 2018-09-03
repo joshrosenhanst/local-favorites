ed19d20c1e16aaa2ffd646cf31d8dccdd5cb45a0
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-09-03 15:52:54 -0400

    clean up spacing on StarRating

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="star-rating">
-        <span class="star-rating-icon star"
-            v-for="n in 5"
-            v-bind:key="'star_'+n"
-            v-bind:class="[stars >= n ? 'full' : 'empty', (currentHover >= n && !readonly) ? 'is-hovered':'', readonly ? 'readonly' : 'is-editable']"
-            v-on:click="setStars(n)"
-            v-on:mouseenter="currentHover = n"
-            v-on:mouseleave="currentHover = 0"
-            v-bind:title="readonly? (n+' stars'):(`Rate ${n} stars`)"
-        >
-            <font-awesome-icon v-bind:icon="[((stars >= n)||(currentHover >= n && !readonly))?'fas':'far','star']" class="star-icon"></font-awesome-icon>
-        </span>
-        <span class="star-rating-icon ban-icon" title="Remove Star Rating"
-            v-if="stars && !readonly"
-            v-on:click="setStars(0)"
-        >
-            <font-awesome-icon icon="ban"></font-awesome-icon>
-        </span>
-    </div>
+  <div class="star-rating">
+    <span class="star-rating-icon star"
+      v-for="n in 5"
+      v-bind:key="'star_'+n"
+      v-bind:class="[stars >= n ? 'full' : 'empty', (currentHover >= n && !readonly) ? 'is-hovered':'', readonly ? 'readonly' : 'is-editable']"
+      v-on:click="setStars(n)"
+      v-on:mouseenter="currentHover = n"
+      v-on:mouseleave="currentHover = 0"
+      v-bind:title="readonly? (n+' stars'):(`Rate ${n} stars`)"
+    >
+      <font-awesome-icon v-bind:icon="[((stars >= n)||(currentHover >= n && !readonly))?'fas':'far','star']" class="star-icon"></font-awesome-icon>
+    </span>
+    <span class="star-rating-icon ban-icon" title="Remove Star Rating"
+      v-if="stars && !readonly"
+      v-on:click="setStars(0)"
+    >
+      <font-awesome-icon icon="ban"></font-awesome-icon>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'StarRating',
-    props: {
-        stars: Number,
-        readonly: Boolean
-    },
-    data: function () {
-        return {
-            currentHover: 0
-        }
-    },
-    methods: {
-        setStars: function (value) {
-            if(!this.readonly){
-                this.$emit('set-stars', { value: value });
-            }
-        }
+  name: 'StarRating',
+  props: {
+    stars: Number,
+    readonly: Boolean
+  },
+  data: function () {
+    return {
+      currentHover: 0
     }
+  },
+  methods: {
+    setStars: function (value) {
+      if(!this.readonly){
+        this.$emit('set-stars', { value: value });
+      }
+    }
+  }
 }
 </script>
 
